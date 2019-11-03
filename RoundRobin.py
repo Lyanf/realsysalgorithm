ffdde640a2cd265ee1ff1fb45814de02221655ff
lyf <609136080@qq.com>
2019-11-03 13:54:19 +0800

    add RoundRobin

---
+++ RoundRobin.py
@@ -0,0 +1,21 @@
+from task import Scheduler,SchedulResult
+
+
+class RoundRobin(Scheduler):
+    def __init__(self):
+        super(RoundRobin, self).__init__()
+
+    def work(self,time):
+        remainedTime = time
+        while remainedTime>0:
+            for task in self.tasks:
+                if remainedTime<=0:
+                    break
+                if not task.isCompleted():
+                    duration = 1
+                    task.work(1)
+                    remainedTime = remainedTime-1
+                    if len(self.results) == 0:
+                        self.results.append(SchedulResult(task.pid, task.arrivalTime, duration))
+                    else:
+                        self.results.append(SchedulResult(task.pid, self.results[-1].endTime, duration))
